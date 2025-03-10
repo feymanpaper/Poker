8c1f1ae6c5e52900e219267e0c6cb7da73c01829
feymanpaper <feymanpaper@gmail.com>
2025-03-10 14:31:36 +0800

    修改runtime_get_screen.py来直接获取当前界面的弹框context

--- runtime_get_screen.py
+++ runtime_get_screen.py
@@ -9,12 +9,11 @@ import uiautomator2 as u2
 def getCurrentPackageName():
     d = u2.connect()
     info = d.info
+    print(info)
     return info['currentPackageName']
 
-def get_pop_up(appName):
+def get_pop_up():
     try:
-        Config.get_instance().target_pkg_name = getCurrentPackageName()
-        Config.get_instance().app_name = appName
         content = get_screen_content()
         cur_activity = content["cur_activity"]
         screenshot_path = content["screenshot_path"]
@@ -39,22 +38,7 @@ def get_pop_up(appName):
 
 if __name__ == '__main__':
     rerun_uiautomator2()
-    appName='默认值'
-    firsttime=True
-    while True:
-        if firsttime:
-            user_input = input("请输入命令（输入【1】执行弹框收集，输入【exit】退出）：")
-            firsttime=False
-        else:
-            user_input = input("请输入命令（输入【1】执行【新应用】弹框收集，输入【2】执行【相同应用】弹框收集，输入【exit】退出）：")
-
-        if user_input == '1':
-            appName = input("请输入应用名：")
-            get_pop_up(appName)
-        elif user_input == '2':
-            get_pop_up(appName)
-        elif user_input.lower() == 'exit':
-            print("退出程序")
-            break
-        else:
-            print("无效输入，请输入【1】或【2】执行程序，或输入【exit】退出。")
+    curPkgName = getCurrentPackageName()
+    Config.get_instance().target_pkg_name = curPkgName
+    Config.get_instance().app_name = curPkgName
+    get_pop_up()
